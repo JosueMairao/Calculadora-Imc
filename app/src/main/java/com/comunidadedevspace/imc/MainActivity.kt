05fd0cfb32741ece3eff2f721f79c7317a2bf668
Mairao <josue.sp@hotmail.com>
2025-03-10 17:12:26 +0100

    clean code

--- app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
+++ app/src/main/java/com/comunidadedevspace/imc/MainActivity.kt
@@ -45,9 +45,7 @@ class MainActivity : AppCompatActivity() {
                 intent.putExtra(KEY_RESULT_IMC, resultado)
              startActivity(intent)
 
-            // cores
-            // Editext background + Icone
-            // Gradiente + icone + titulo + descricao
+
              }
          }
 
